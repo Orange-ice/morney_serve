9044906072641c1f5d81f066ba4f6fc481539264
Burt <2902978956@qq.com>
2021-05-15 22:41:47 +0800

    使用 sequelize-cli 初始化数据库和 Migrations

---
+++ database/migrations/20210515135627-init-users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    const { INTEGER, DATE, STRING } = Sequelize
+    await queryInterface.createTable('users', {
+      id: { type: INTEGER, primaryKey: true, autoIncrement: true },
+      username: STRING,
+      password: STRING,
+      avatar: STRING,
+      created_at: DATE,
+      updated_at: DATE
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('users')
+  }
+};
