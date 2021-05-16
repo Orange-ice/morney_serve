35651c4d6c4216f828d80efca770cc0cadbdccbc
Burt <2902978956@qq.com>
2021-05-16 09:45:02 +0800

    fix egg-sequelize error access denied

--- config/config.default.js
+++ config/config.default.js
@@ -18,6 +18,8 @@ module.exports = appInfo => {
   config.sequelize = {
     dialect: 'mysql',
     host: '127.0.0.1',
+    username: 'root',
+    password: '123456',
     port: 3306,
     database: 'egg-sequelize-default'
   };
